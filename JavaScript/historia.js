0af1024838d9d1029bc5913db864c03d6435a59e
Leandro Moreno <morenoleandro182@gmail.com>
2025-10-26 22:42:47 -0300

    Datos a seguir para el event

---
+++ JavaScript/historia.js
@@ -0,0 +1,11 @@
+/*
+1) Crear un evento por Div padre 1,2 y 3.
+2) Manejar las animaciones por clase añadidas por el evento a base de scroll.
+3) Las animaciones deben tener: animation-name: ;
+    animation-duration: 4s y 3s;
+    animation-timing-function: cubic-bezier.
+    animation-fill-mode: forwards;
+/*
+window.addEventListener('scroll' , function () {
+    const 
+}
