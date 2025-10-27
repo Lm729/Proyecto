c469806291785e511a58f7b4e244b7064e7b52de
Leandro Moreno <morenoleandro182@gmail.com>
2025-10-26 22:43:23 -0300

    Cambio de let a const

--- JavaScript/general.js
+++ JavaScript/general.js
@@ -1,8 +1,8 @@
 window.addEventListener('scroll' , function () {
-    let nav= document.querySelector('nav');
-    let links= document.querySelectorAll('.link');
+    const nav = document.querySelector('nav');
+    const links = document.querySelectorAll('.link');
 
-    if (window.scrollY >= window.innerHeight*0.1) {
+    if (window.scrollY >= window.innerHeight *0.1) {
         nav.classList.add('navScroll');
         links.forEach(link => link.classList.add('linkColor'));
     }
